26f203b73ec797b709d7346c53f3e9d27e3d295d
Adam Wellington <adam3654@gmail.com>
2022-10-04 16:18:50 -0400

    comment control import

--- octoprint_octolight/__init__.py
+++ octoprint_octolight/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
-import octoprint.control
+#import octoprint.control
 
 from octoprint.events import Events
 import flask
